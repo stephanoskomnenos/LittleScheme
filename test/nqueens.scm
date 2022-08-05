97c9992aae0698acf53a1e3fc87d7bf727fb16ab
Steven Lin <linjt267@outlook.com>
2022-08-05 20:35:44 +0800

    add stdlib

---
+++ test/nqueens.scm
@@ -0,0 +1,58 @@
+(load "stdlib.scm")
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+            (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens))
+
+(define (safe? k positions)
+  (define (iter-check row-of-new-queen rest-of-queens i)
+    (if (null? rest-of-queens)
+        #t
+        (let ((row-of-current-queen (car rest-of-queens)))
+          (if (or (= row-of-new-queen row-of-current-queen)
+                  (= row-of-new-queen (+ i row-of-current-queen))
+                  (= row-of-new-queen (- row-of-current-queen i)))
+              #f
+              (iter-check row-of-new-queen
+                          (cdr rest-of-queens)
+                          (+ i 1))))))
+  (iter-check (car positions)
+              (cdr positions)
+              1))
+
+(queens 8)
